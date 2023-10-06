635098e9350038c3d9ad1f92592038bda10a6a64
Ben khalifa Ghada <benkhalifaghada@gmail.com>
2023-10-06 15:28:37 +0100

    correction d'atelier Twig et ORM_part1

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,12 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
+    public $authors = array(
+
+
+        array(
+            'id' => 1, 'picture' => '/images/Victor-Hugo.jpg',
+            'username' => ' Victor Hugo', 'email' => 'victor.hugo@gmail.com ', 'nb_books' => 100
+        ),
+        array(
+            'id' => 2, 'picture' => '/images/william-shakespeare.jpg',
+            'username' => ' William Shakespeare', 'email' => ' william.shakespeare@gmail.com', 'nb_books' => 200
+        ),
+        array(
+            'id' => 3, 'picture' => '/images/Taha_Hussein.jpg',
+            'username' => ' Taha Hussein', 'email' => 'taha.hussein@gmail.com', 'nb_books' => 300
+        ),
+    );
     #[Route('/author', name: 'app_author')]
     public function index(): Response
     {
@@ -19,5 +36,36 @@ class AuthorController extends AbstractController
     #[Route('/author/{name}', name:'showAuthor')]
     public function showAuthor($name){
         return $this->render('author/showAuthor.html.twig',['name'=>$name]);
+    }
+
+    // Exercice 2 : Manipulation du tableau associatif, filtre et Structure conditionnelle
+    #[Route('/list', name: 'list_author')]
+    public function list()
+    {
+        return $this->render('author/listA.html.twig', [
+            'authors' => $this->authors,
+        ]);
+    }
+
+    #[Route('/author/{id}', name: 'author_details')]
+    public function authorDetails($id)
+    {
+        $author = null;
+        // Parcourez le tableau pour trouver l'auteur correspondant à l'ID
+        foreach ($this->authors as $authorData) {
+            if ($authorData['id'] == $id) {
+                $author = $authorData;
+            };
+        };
+        return $this->render('author/showA.html.twig', [
+            'author' => $author,
+            'id' => $id
+        ]);
+    }
+
+    #[Route('/listA',name: 'listAuteurs')]
+    public function listAuteurs(AuthorRepository $repo){
+       $list=$repo->findAll();
+       return $this->render('author/list.html.twig',['authors'=>$list]);
     }
 }
